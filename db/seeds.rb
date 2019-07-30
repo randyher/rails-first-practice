672e3eda713318449d122db8fe2ce1f3023f4a03
github username <github email address>
2019-07-30 14:33:19 -0400

    add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+one = CatSitter.create(name: "Jon", age: 26)
+two = CatSitter.create(name: "Dany", age: 22)
+three = CatSitter.create(name: "Robert", age: 25)
+four = CatSitter.create(name: "Theo", age: 18)
+
+Cat.create(name: "Theo", fur_color: "Red", cat_sitter: one)
+Cat.create(name: "Nana", fur_color: "Gold", cat_sitter: one)
+Cat.create(name: "Lala", fur_color: "Black", cat_sitter: two)
+Cat.create(name: "Mama", fur_color: "Brown", cat_sitter: two)
+Cat.create(name: "Shot", fur_color: "Brown", cat_sitter: three)
+Cat.create(name: "Runny", fur_color: "Brown", cat_sitter: three)
+Cat.create(name: "Rabbit", fur_color: "Red", cat_sitter: four)
+Cat.create(name: "Turtle", fur_color: "White", cat_sitter: four)
